97cbd21d869fc75b449bb7c71890799b6b87d4e0
sujkovic <adrian.sujkovic@gmail.com>
2022-07-06 13:59:15 -0400

    replace book function with class

--- main.js
+++ main.js
@@ -4,50 +4,52 @@ let bookObj;
 let bookIndex = 0;
 let myLibrary = [];
 
-function Book(title, author, pages, read, index) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.index = index;
-    bookDiv = document.createElement('div');
-    bookNameDiv = document.createElement('div');
-    bookAuthorDiv = document.createElement('div');
-    bookPagesDiv = document.createElement('div');
-    let bookReadButton = document.createElement('button');
-    bookReadButton.classList.add('readbtn');
-    let removeButton = document.createElement('button');
-    bookNameDiv.textContent = title;
-    bookAuthorDiv.textContent = author;
-    bookPagesDiv.textContent = pages;
-    bookReadButton.textContent = read;
-    bookReadButton.addEventListener('click', () => {
-        if (this.read === 'read') {
-            bookReadButton.textContent = 'not read';
-            this.read = 'not read';
-        }
-        else if (this.read === 'not read') {
-            bookReadButton.textContent = 'read';
-            this.read = 'read';
-        }
-    });
-    removeButton.classList.add('remove-button');
-    removeButton.classList.add(`${bookIndex}`);
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-        myLibrary.splice(this.bookIndex, 1);
-        removeButton.parentElement.remove();
-    });
-    bookDiv.appendChild(bookNameDiv);
-    bookDiv.appendChild(bookAuthorDiv);
-    bookDiv.appendChild(bookPagesDiv);
-    bookDiv.appendChild(bookReadButton);
-    bookDiv.appendChild(removeButton);
-    bookDiv.classList.add('book');
-    this.div = bookDiv;
-    libraryWrapper.appendChild(bookDiv);
-    removeButtons = document.querySelectorAll('.remove-button');
-    bookIndex++;
+class Book {
+    constructor(title, author, pages, read, index) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.index = index;
+        bookDiv = document.createElement('div');
+        bookNameDiv = document.createElement('div');
+        bookAuthorDiv = document.createElement('div');
+        bookPagesDiv = document.createElement('div');
+        let bookReadButton = document.createElement('button');
+        bookReadButton.classList.add('readbtn');
+        let removeButton = document.createElement('button');
+        bookNameDiv.textContent = title;
+        bookAuthorDiv.textContent = author;
+        bookPagesDiv.textContent = pages;
+        bookReadButton.textContent = read;
+        bookReadButton.addEventListener('click', () => {
+            if (this.read === 'read') {
+                bookReadButton.textContent = 'not read';
+                this.read = 'not read';
+            }
+            else if (this.read === 'not read') {
+                bookReadButton.textContent = 'read';
+                this.read = 'read';
+            }
+        });
+        removeButton.classList.add('remove-button');
+        removeButton.classList.add(`${bookIndex}`);
+        removeButton.textContent = 'Remove';
+        removeButton.addEventListener('click', () => {
+            myLibrary.splice(this.bookIndex, 1);
+            removeButton.parentElement.remove();
+        });
+        bookDiv.appendChild(bookNameDiv);
+        bookDiv.appendChild(bookAuthorDiv);
+        bookDiv.appendChild(bookPagesDiv);
+        bookDiv.appendChild(bookReadButton);
+        bookDiv.appendChild(removeButton);
+        bookDiv.classList.add('book');
+        this.div = bookDiv;
+        libraryWrapper.appendChild(bookDiv);
+        removeButtons = document.querySelectorAll('.remove-button');
+        bookIndex++;
+    }
 }
 
 let newBookButton = document.querySelector('.btn');
